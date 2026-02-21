0c412ec0287985d8d4f9724096d29450d2b958ad
Frank Stein <dr.frank.stain@gmail.com>
2026-02-21 16:42:04 +0300

    Introduce the namespace of library.

---
+++ include/.bootstrap/namespace.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+// Black Framework main namespace.
+namespace Black
+{
+// Data compression library.
+inline namespace Compression
+{
+// Internal namespace for compression library.
+namespace Internal
+{
+
+}
+}
+}
