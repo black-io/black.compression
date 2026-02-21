15f292129d1eab2067aed6dcb392c35a2d8282d8
Frank Stein <dr.frank.stain@gmail.com>
2026-02-21 16:42:39 +0300

    Introduce the first code of compressor options.

---
+++ include/.global/compression/internal/constants.h
@@ -0,0 +1,31 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Compression
+{
+inline namespace Global
+{
+inline namespace Compression
+{
+namespace Internal
+{
+	/**
+		@brief	Enumeration of posable compression options.
+	*/
+	enum class CompressionOptionId : uint32_t
+	{
+	};
+
+	/**
+		@brief	Enumeration of posable decompression options.
+	*/
+	enum class DecompressionOptionId : uint32_t
+	{
+	};
+}
+}
+}
+}
+}
