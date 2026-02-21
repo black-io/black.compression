6acdd2bc21c57a32a3cd78077d86d0d4dd439b1e
Frank Stein <dr.frank.stain@gmail.com>
2026-02-21 16:46:51 +0300

    Add regular library headers.

---
+++ include/.epilogue.h
@@ -0,0 +1,4 @@
+#pragma once
+
+
+// Deferred inline implementations.
---
+++ include/.prologue.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+// External dependencies.
+#include <black/core.h>
+
+
+// The library namespace definition.
+#include ".bootstrap/namespace.h"
+
+// Most global stuff.
+
+// Generic compression algorithm.
+#include ".global/compression/internal/constants.h"
+#include ".global/compression/internal/structures.h"
+
+#include ".global/compression/BasicCompressor.h"
