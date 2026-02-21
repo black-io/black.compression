f845eeb6034547398584d3a8a1b70721a18ac5b2
Frank Stein <dr.frank.stain@gmail.com>
2026-02-21 16:43:00 +0300

    Compressor options implemented.

---
+++ include/.global/compression/internal/structures.h
@@ -0,0 +1,43 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Compression
+{
+inline namespace Global
+{
+inline namespace Compression
+{
+namespace Internal
+{
+	/**
+		@brief	Regular option for compression algorithm.
+	*/
+	struct CompressionOption final
+	{
+		// Type of option identifier.
+		using Id = CompressionOptionId;
+
+
+		Id			id;		// Id used to identify the semantics of value.
+		uint32_t	value;	// Usable value.
+	};
+
+	/**
+		@brief	Regular option for decompression algorithm.
+	*/
+	struct DecompressionOption final
+	{
+		// Type of option identifier.
+		using Id = DecompressionOptionId;
+
+
+		Id			id;		// Id used to identify the semantics of value.
+		uint32_t	value;	// Usable value.
+	};
+}
+}
+}
+}
+}
