5b0e80bee580b55d4356c2bdccd6a22f425e475d
Frank Stein <dr.frank.stain@gmail.com>
2026-02-23 20:19:26 +0300

    Add the headers of compressor registry.

--- include/.epilogue.h
+++ include/.epilogue.h
@@ -2,3 +2,6 @@
 
 
 // Deferred inline implementations.
+#include ".global/compression/CompressorRegistry.inl"
+
+#include ".global/compression/configuration/configuration-list-items.inl"
--- include/.prologue.h
+++ include/.prologue.h
@@ -2,7 +2,8 @@
 
 
 // External dependencies.
-#include <black/core.h>
+#include <black/core/algorithms.h>
+#include <black/core/static-lists.h>
 
 
 // The library namespace definition.
@@ -15,3 +16,9 @@
 #include ".global/compression/internal/structures.h"
 
 #include ".global/compression/BasicCompressor.h"
+
+// Compressor registry.
+#include ".global/compression/configuration/configuration-list-items.h"
+#include ".global/compression/configuration/aliases.h"
+#include ".global/compression/configuration/configuration-options.h"
+#include ".global/compression/CompressorRegistry.h"
